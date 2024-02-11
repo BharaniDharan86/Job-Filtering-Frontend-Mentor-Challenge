a1c33abffda4316f0b3cbec92d1842993a3bf302
bharanidev-001 <bharanidharanm77@gmail.com>
2024-02-11 12:46:40 +0530

    filter button

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -4,7 +4,23 @@ import JobFilter from "./JobFilter";
 import JobList from "./JobList";
 function Jobs() {
   const { jobs: allJobs, filteredJob } = useJobContext();
-  const jobs = filteredJob.length === 0 ? allJobs : allJobs;
+  const jobs =
+    filteredJob.length === 0
+      ? allJobs
+      : allJobs.filter((job) => {
+          return filteredJob.some((filter) => {
+            return (
+              job.position.toLowerCase().includes(filter.toLowerCase()) ||
+              job.role.toLowerCase() === filter.toLowerCase() || // Corrected this line
+              job.languages.some(
+                (language) => language.toLowerCase() === filter.toLowerCase()
+              ) ||
+              job.tools.some(
+                (tool) => tool.toLowerCase() === filter.toLowerCase()
+              )
+            );
+          });
+        });
 
   return (
     <div className=" mb-4 mt-[40px] max-w-[900px] mx-auto">
